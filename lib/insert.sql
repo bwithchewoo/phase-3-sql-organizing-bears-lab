16ac1d286481e77c234fbeea7445859c9fcffcf3
Anna Cho <loveablessing@gmail.com>
2022-12-09 19:28:58 -0500

    commit tree 443ffef632e9f4e6677888bd8b3034d0e89999a2

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 18, "M", "BROWN", "MEAN", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 19, "M", "RED", "PASSIONATE", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 20, "F", "TAN", "QUIET", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 21, "M", "BROWN", "MILITANT", 0);
+INSERT INTO bears VALUES (5, "Melissa", 22, "F", "PURPLE", "HAPPY", 1);
+INSERT INTO bears VALUES (6, "Grinch", 23, "M", "GREEN", "GRUMPY", 1);
+INSERT INTO bears VALUES (7, "Wendy", 24, "F", "WHITE", "CAPITALIST", 0);
+INSERT INTO bears VALUES (8, null, 25, "M", "BLACK", "NAIVE", 1);
